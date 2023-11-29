11a521135e3c39641f159e550c52127f47703afe
fergrenteski <luizgrenfer@gmail.com>
2023-11-29 15:22:43 -0300

    onebit code

---
+++ JAVASCRIPT/exercicio 6.1/script.js
@@ -0,0 +1,9 @@
+let palavra = prompt("Digite uma palavra:")
+
+for(i = 0; i < palavra.length; i++){
+    for(j = palavra.length; j > 0; j--) {
+        if(palavra[i] !== palavra[j]) {
+           
+        }
+    }
+} alert("é um Palíndromo")
